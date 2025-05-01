520c777bd52386a11237e5c5a8e2e24c2cc0b6f8
hkY333 <kimabc0630@naver.com>
2025-05-01 16:12:41 +0900

    2

--- ScoreUI.cs
+++ ScoreUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,25 +12,23 @@ public class ScoreUI : BaseUI
     Button startButton;
     Button exitButton;
 
-    protected override UIState GetUIStates()
-    {
-        return UIState.Score;
-    }
-
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
-
         scoreText = transform.Find("ScoreText").GetComponent<TextMeshProUGUI>();
         comboText = transform.Find("ComboText").GetComponent<TextMeshProUGUI>();
         bestScoreText = transform.Find("BestScoreText").GetComponent<TextMeshProUGUI>();
         bestComboText = transform.Find("BestComboText").GetComponent<TextMeshProUGUI>();
-
         startButton = transform.Find("StartButton").GetComponent<Button>();
         exitButton = transform.Find("ExitButton").GetComponent<Button>();
 
         startButton.onClick.AddListener(OnClickStartButton);
         exitButton.onClick.AddListener(OnClickExitButton);
+    }
+
+    protected override UIState GetUIState()
+    {
+        return UIState.Score;
     }
 
     public void SetUI(int score, int combo, int bestScore, int bestCombo)
@@ -41,13 +37,13 @@ public class ScoreUI : BaseUI
         comboText.text = combo.ToString();
         bestScoreText.text = bestScore.ToString();
         bestComboText.text = bestCombo.ToString();
-
     }
 
     void OnClickStartButton()
     {
         uiManager.OnClickStart();
     }
+
     void OnClickExitButton()
     {
         uiManager.OnClickExit();
