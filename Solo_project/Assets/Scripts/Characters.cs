8462911f2d9ba83d5141453b9310be65a902db43
hkY333 <kimabc0630@naver.com>
2025-05-07 03:18:03 +0900

    타일맵 만들기, 스크립트 만들기

--- Solo_project/Assets/Scripts/Characters.cs
+++ Solo_project/Assets/Scripts/Characters.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Characters : MonoBehaviour
+public abstract class Characters : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Move();
 }
