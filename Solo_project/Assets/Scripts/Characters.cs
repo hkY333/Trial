83ee460fac3bdf1af49befcd7280fb86919d0d07
hkY333 <kimabc0630@naver.com>
2025-05-07 12:50:51 +0900

    카메라 재설정, Characters, Player 스크립트 수정

--- Solo_project/Assets/Scripts/Characters.cs
+++ Solo_project/Assets/Scripts/Characters.cs
@@ -4,6 +4,74 @@ using UnityEngine;
 
 public abstract class Characters : MonoBehaviour
 {
+    protected Rigidbody2D _rigidbody;
 
-    public abstract void Move();
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movment(movementDirection);
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+
+    protected abstract void Move();
+
+    private void Movment(Vector2 direction)
+    {
+        direction = direction * 5;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
+
+        //if (weaponPivot != null)
+        //{
+        //    weaponPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        //}
+    }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;
+    }
 }
--- Solo_project/Assets/Scripts/Player.cs
+++ Solo_project/Assets/Scripts/Player.cs
@@ -1,18 +1,45 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Characters
 {
+    
+
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         
     }
+
+    protected override void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePosition);
+        
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+
+
 }
